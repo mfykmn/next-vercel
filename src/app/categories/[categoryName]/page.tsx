3dafbbe4a7d89e8c82d9c4fa6a6020409bb38332
mfykmn <mfykmn@gmail.com>
2024-12-22 23:28:21 +0900

    update

--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,7 +1,15 @@
-export default function CategoryPage() {
+type Props = {
+    params: {categoryName: string};
+    searchParams: {[key: string]: string | string[] | undefined};
+}
+
+export default function Page({params, searchParams}: Props) {
+    const page = typeof searchParams.page === 'string' ? searchParams.page : '1';
     return (
         <div>
-            <h1>カテゴリページ</h1>
+            <h1>カテゴリ別一覧画面</h1>
+            <p>カテゴリ名: {params.categoryName}</p>
+            <p>ページ: {page}</p>
         </div>
     )
 }
