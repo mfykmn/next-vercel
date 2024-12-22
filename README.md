18001fe59d5a7613d0115827c3260246aa933a1c
mfykmn <mfykmn@gmail.com>
2024-12-23 01:24:57 +0900

    update

--- README.md
+++ README.md
@@ -25,19 +25,23 @@ npx vercel
 
 ## メモ
 ### React Server Components(RSC)
-デメリットとしては、以下の2つがあります。
+Server Componentsのデメリットとしては、以下の2つがあります。
 
-ブラウザAPIが使えない
-useEffectなどReactのフックが使用できない
+- ブラウザAPIが使えない
+- useEffectなどReactのフックが使用できない
 
+ファイルの先頭に`"use client"`をつけると、そのファイルはClient Componentsとして扱われる
 
-
+ここらへんの理解があやふやなので、もう少し調べる
+https://zenn.dev/luvmini511/articles/ec0e874a2cc1f1
 
 ### ナビゲーション
 Next.jsのSPAナビゲーションはソフトナビゲーションで、ブラウザは画面を再読込せずに必要な箇所のみ再レンダリングする
 App Routerでは、変更されたRoute Segmentのみ再レンダリングする
 
 Next.jsでは、<Link>コンポーネントとuseRouter Hookでナビゲーションをすることになるので、その使い分けを理解する
+
+open http://localhost:3000/categories/flower?page=2
 
 ## 参考
 - https://zenn.dev/akfm/books/nextjs-basic-principle
--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,3 +1,6 @@
+// "use client";
+// import { useRouter } from "next/router";
+
 type Props = {
     params: {categoryName: string};
     searchParams: {[key: string]: string | string[] | undefined};
@@ -5,11 +8,20 @@ type Props = {
 
 export default function Page({params, searchParams}: Props) {
     const page = typeof searchParams.page === 'string' ? searchParams.page : '1';
+    //const router = useRouter();
+
     return (
         <div>
             <h1>カテゴリ別一覧画面</h1>
             <p>カテゴリ名: {params.categoryName}</p>
             <p>ページ: {page}</p>
+            {/* <button onClick={
+                () => {
+                    router.push('/categories');
+                }
+            }>
+                カテゴリ一覧へ戻る
+            </button> */}
         </div>
     )
 }
