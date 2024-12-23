93e94865bd0635e496b401fe813b107fbe4a704e
mfykmn <mfykmn@gmail.com>
2024-12-24 01:18:09 +0900

    update

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
 
-export default function Page() {
+async function getCategories() {
+    const data = await fetch(
+        "https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060",
+    ).then(res => res.json());
+    return data;
+}
+
+export default async function Page() {
+    // APIを並列で取得することで、パフォーマンスを向上させることができる
+    const [categories, categories2] = await Promise.all([
+        getCategories(),
+        getCategories(),
+    ]);
+    console.log(categories);
+    console.log(categories2);
+
     return (
         <div>
         <h1>カテゴリ一覧</h1>
